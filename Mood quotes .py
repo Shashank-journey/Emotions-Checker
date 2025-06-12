ac3021a00c2efec49935604aff8d228e79fde2a4
Shashank Shekhar <shashankshekhar.shyam@gmail.com>
2025-06-12 10:37:19 +0530

    Add files via upload
    It is a beginner level python  script responds based on your mood 
    Built entirely on mobile using Pydroid3

---
+++ Mood quotes .py
@@ -0,0 +1,11 @@
+mood= input("How are you feeling right now?(motivated/happy/sad)")
+if mood== "happy" :
+  print("Keep that smile on ur face its the most valuable treasute you can have ")
+elif mood== "motivated":
+  print("Stay motivated champ,nothing can stop you and remember motivation by your inner self can bring a change ")
+elif mood == "sad": 
+  print("Dont let that rain in form greif take over u,try to keep a smile ,dont loose hope ")
+else:
+  print("emotions are the data which ai is also not able to replicate")
+    
+
